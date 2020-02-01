a5e26bad2230226d067600dd545ca87e4f8b0566
Alexander Tinyaev <dix.alex@gmail.com>
2020-02-01 08:52:20 +0200

    Add Ads API

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const accessHeader = require('./middleware/access-headers');
 
 const usersRoute = require("./routes/users.route");
 const threadsRoute = require("./routes/threads.route");
+const adsRoute = require("./routes/ads.route");
 
 if (!config.get("myprivatekey")) {
   console.error("FATAL ERROR: myprivatekey is not defined.");
@@ -23,6 +24,7 @@ server.use(jsonServer.bodyParser)
 
 server.use("/api/users", usersRoute);
 server.use("/api/threads", threadsRoute);
+server.use("/api/ads", adsRoute);
 
 server.use(router);
 
